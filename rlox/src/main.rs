89df2bd4d3fa2df3899f9c6fda8a8bdda2e4ffb4
Juan Alvarez <j@yabit.io>
2021-02-16 10:13:47 -0600

    init chunk and opcode

--- rlox/src/main.rs
+++ rlox/src/main.rs
@@ -1,3 +1,68 @@
+enum OpCode {
+    Return,
+    Unknown,
+}
+
+impl From<u8> for OpCode {
+    fn from(n: u8) -> Self {
+        match n {
+            0 => OpCode::Return,
+            _ => OpCode::Unknown,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Chunk {
+    code: Vec<u8>,
+}
+
+impl Chunk {
+    pub fn new() -> Self {
+        Chunk { code: Vec::new() }
+    }
+
+    fn write(&mut self, op: OpCode) {
+        self.code.push(op as u8);
+    }
+
+    fn len(&self) -> usize {
+        self.code.len()
+    }
+}
+
+impl Chunk {
+    fn disassemble(&self, name: &str) {
+        println!("== {} ==", name);
+
+        let mut offset = 0;
+        while offset < self.len() {
+            offset = self.disassemble_instruction(offset);
+        }
+    }
+
+    fn disassemble_instruction(&self, offset: usize) -> usize {
+        print!("{:04} ", offset);
+
+        let op: OpCode = self.code[offset].into();
+
+        match op {
+            OpCode::Return => self.simple_instruction("OP_RETURN", offset),
+            OpCode::Unknown => {
+                println!("Unknown opcode {}", self.code[offset]);
+                offset + 1
+            }
+        }
+    }
+
+    fn simple_instruction(&self, name: &str, offset: usize) -> usize {
+        println!("{}", name);
+        offset + 1
+    }
+}
+
 fn main() {
-    println!("rlox");
+    let mut chunk = Chunk::new();
+    chunk.write(OpCode::Return);
+    chunk.disassemble("test chunk");
 }
